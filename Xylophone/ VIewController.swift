fa8859c881753973dce5b57fb60e05fd58ebba71
Gerson <gerson.lcc@hotmail.com>
2017-09-29 11:37:58 +0100

    Array and function

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -13,14 +13,22 @@ class ViewController: UIViewController, AVAudioPlayerDelegate{
     
     var audioPlayer: AVAudioPlayer!
     
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
+        var selectedSoundNote: String = soundArray[sender.tag - 1]
+        playSound()
+        print(selectedSoundNote)
+    }
+    
+    func playSound() {
         
-    let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
-     
+        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+        
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
             
@@ -29,8 +37,6 @@ class ViewController: UIViewController, AVAudioPlayerDelegate{
         }
         audioPlayer.play()
     }
-    
-  
 
 }
 
